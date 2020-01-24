e74866c2a9155b1b759390d54d4f7ef265bdb01c
Matthew LaFalce <mlafalce206@gmail.com>
2020-01-24 13:43:25 -0500

    Define main three functions
    - Calculate
    - Reset
    - Backspace

--- src/App.js
+++ src/App.js
@@ -12,6 +12,29 @@ class App extends Component {
     }
   }
 
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0,-1)
+    })
+  };
 
   render() {
     return (
